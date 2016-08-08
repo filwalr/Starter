108e65d9a5a44d23887203ecea1e95fec3da0f06
filwalr <wallerp@gmail.com>
2016-08-07 21:49:48 -0400

    add controller function w/ scope service and geolocation service to javascript

--- www/js/app.js
+++ www/js/app.js
@@ -5,6 +5,10 @@
 // the 2nd parameter is an array of 'requires'
 angular.module('starter', ['ionic'])
 
+app.controller('LocationCtrl', function($scope, $cordovaGeolocation){
+  
+})
+
 .run(function($ionicPlatform) {
   $ionicPlatform.ready(function() {
     if(window.cordova && window.cordova.plugins.Keyboard) {

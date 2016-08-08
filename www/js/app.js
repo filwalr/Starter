413aafa59c4b39fcbec06618a670c97143b3b32b
filwalr <wallerp@gmail.com>
2016-08-07 22:03:04 -0400

    bring in example code from ng cordova documentation calling long/lat coordinates.  see first example at http://ngcordova.com/docs/plugins/geolocation/

--- www/js/app.js
+++ www/js/app.js
@@ -6,6 +6,19 @@
 angular.module('starter', ['ionic'])
 
 app.controller('LocationCtrl', function($scope, $cordovaGeolocation){
+
+module.controller('GeoCtrl', function($cordovaGeolocation) {
+
+  var posOptions = {timeout: 10000, enableHighAccuracy: false};
+  $cordovaGeolocation
+    .getCurrentPosition(posOptions)
+    .then(function (position) {
+      var lat  = position.coords.latitude
+      var long = position.coords.longitude
+    }, function(err) {
+      // error
+    });
+
   
 })
 
